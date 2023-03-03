b6a65a6d75855dca3fd9456d26a3a8c74778b9e6
takagi-sh <takagi-sh@PC-4266.local>
2023-03-03 21:10:42 +0900

    first commit

---
+++ epic-nfts/scripts/deploy.js
@@ -0,0 +1,27 @@
+// deploy.js
+const main = async () => {
+  // コントラクトがコンパイルします
+  // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  // Hardhat がローカルの Ethereum ネットワークを作成します。
+  const nftContract = await nftContractFactory.deploy();
+  // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
+  let txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待ちます。
+  await txn.wait();
+  console.log("Minted NFT #1");
+};
+// エラー処理を行っています。
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
