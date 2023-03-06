bac3bae7bb0ce95eef12bdc9e29a664da6a11d63
takagi-sh <takagi-sh@blocksmithand.co.jp>
2023-03-06 23:03:53 +0900

    second commit

--- epic-nfts/contracts/MyEpicNFT.sol
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -10,122 +10,105 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+
 // Base64.solコントラクトからSVGとJSONをBase64に変換する関数をインポートします。
 import { Base64 } from "./libraries/Base64.sol";
 
 // インポートした OpenZeppelin のコントラクトを継承しています。
 // 継承したコントラクトのメソッドにアクセスできるようになります。
-contract MyEpicNFT is ERC721URIStorage {
+contract MyEpicNFT is ERC721URIStorage, Ownable {
   // OpenZeppelin　が　tokenIds　を簡単に追跡するために提供するライブラリを呼び出しています
   using Counters for Counters.Counter;
   // _tokenIdsを初期化（_tokenIds = 0）
   Counters.Counter private _tokenIds;
 
-  // SVGコードを作成します。
-  // 変更されるのは、表示される単語だけです。
-  // すべてのNFTにSVGコードを適用するために、baseSvg変数を作成します。
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  // // SVGコードを作成します。
+  // // 変更されるのは、表示される単語だけです。
+  // // すべてのNFTにSVGコードを適用するために、baseSvg変数を作成します。
+  // string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  // 3つの配列 string[] に、それぞれランダムな単語を設定しましょう。
-  string[] firstWords = ["super", "irritating", "binary", "very", "barely", "surprisingly"];
-  string[] secondWords = ["cold", "hot", "cool", "cute", "warm", "wild"];
-  string[] thirdWords = ["cat", "dog", "bird", "sauna", "horse", "flog"];
+  // // 3つの配列 string[] に、それぞれランダムな単語を設定しましょう。
+  // string[] firstWords = ["super", "irritating", "binary", "very", "barely", "surprisingly"];
+  // string[] secondWords = ["cold", "hot", "cool", "cute", "warm", "wild"];
+  // string[] thirdWords = ["cat", "dog", "bird", "sauna", "horse", "flog"];
+
+  // Max batch size for minting one time
+  uint256 private _maxBatchSize;
+  // Mapping from token Id to hashed credential ID
+  mapping(uint256 => string) private _ownedCredential;
+  // Mapping from hashed credential Id to owner possession flag
+  mapping(string => mapping(address => bool)) private _credentialOwnerships;
+
 
 event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   // NFT トークンの名前とそのシンボルを渡します。
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract.");
-  }
-
-  // シードを生成する関数を作成します。
-  function random(string memory input) internal pure returns (uint256) {
-      return uint256(keccak256(abi.encodePacked(input)));
-  }
-
-  // 各配列からランダムに単語を選ぶ関数を3つ作成します。
-  // pickRandomFirstWord関数は、最初の単語を選びます。
-  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-    // pickRandomFirstWord 関数のシードとなる rand を作成します。
-    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    // seed rand をターミナルに出力する。
-	  console.log("rand - seed: ", rand);
-	  // firstWords配列の長さを基準に、rand 番目の単語を選びます。
-    rand = rand % firstWords.length;
-	  // firstWords配列から何番目の単語が選ばれるかターミナルに出力する。
-	  console.log("rand - first word: ", rand);
-    return firstWords[rand];
-  }
-
-  // pickRandomSecondWord関数は、2番目に表示されるの単語を選びます。
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-    // pickRandomSecondWord 関数のシードとなる rand を作成します。
-    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-    rand = rand % secondWords.length;
-    return secondWords[rand];
-  }
-
-  // pickRandomThirdWord関数は、3番目に表示されるの単語を選びます。
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    // pickRandomThirdWord 関数のシードとなる rand を作成します。
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
+  constructor(string memory _name,string memory _symbol,uint256 _newMaxBatchSize) ERC721 (_name, _symbol) {
+    _maxBatchSize = _newMaxBatchSize;
   }
 
   // ユーザーが NFT を取得するために実行する関数です。
-  function makeAnEpicNFT() public {
+  function makeAnEpicNFT(string memory _description,
+        string memory _imageURI,
+        string memory _externalURI) internal view onlyOwner returns(string memory){
     // 現在のtokenIdを取得します。tokenIdは0から始まります。
     uint256 newItemId = _tokenIds.current();
+   
+   
+   
+    uint256 requestNum = _toAddresses.length;
 
-    // 3つの配列からそれぞれ1つの単語をランダムに取り出します。
-    string memory first = pickRandomFirstWord(newItemId);
-    string memory second = pickRandomSecondWord(newItemId);
-    string memory third = pickRandomThirdWord(newItemId);
-
-	  // 3つの単語を連携して格納する変数 combinedWord を定義します。
-    string memory combinedWord = string(abi.encodePacked(first, second, third));
-
-    // 3つの単語を連結して、<text>タグと<svg>タグで閉じます。
-    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
-
-	  // NFTに出力されるテキストをターミナルに出力します。
-	  console.log("\n----- SVG data -----");
-    console.log(finalSvg);
-    console.log("--------------------\n");
+    require(requestNum > 0, "The _toAddresses is empty.");
+    require(
+        requestNum <= getMaxBatchSize(),
+        "The length of _toAddresses must be less than or equal to _maxBatchSize."
+    );
+    require(
+        requestNum == _imageURIs.length,
+        "The length of _toAddresses and _imageURIs are NOT same."
+    );
+    require(
+        requestNum == _externalURIs.length,
+        "The length of _toAddresses and _externalURIs are NOT same."
+    );
+	  // // NFTに出力されるテキストをターミナルに出力します。
+	  // console.log("\n----- SVG data -----");
+    // console.log(finalSvg);
+    // console.log("--------------------\n");
 
     // JSONファイルを所定の位置に取得し、base64としてエンコードします。
+    bytes memory _attributes = abi.encodePacked('"attributes": []');
     string memory json = Base64.encode(
         bytes(
             string(
                 abi.encodePacked(
-                    '{"name": "',
-                    // NFTのタイトルを生成される言葉（例: GrandCuteBird）に設定します。
-                    combinedWord,
-                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    //  data:image/svg+xml;base64 を追加し、SVG を base64 でエンコードした結果を追加します。
-                    Base64.encode(bytes(finalSvg)),
-                    '"}'
+                  '{"name": "',
+                name(),
+                '",'
+                '"description": "',
+                _description,
+                '",'
+                '"image": "',
+                _imageURI,
+                '",',
+                '"external_url": "',
+                _externalURI,
+                '",',
+                string(_attributes),
+                "}"
                 )
             )
         )
     );
 
-    // データの先頭に data:application/json;base64 を追加します。
-    string memory finalTokenUri = string(
-        abi.encodePacked("data:application/json;base64,", json)
-    );
-
-	  console.log("\n----- Token URI ----");
-    console.log(finalTokenUri);
-    console.log("--------------------\n");
-
     // msg.sender を使って NFT を送信者に Mint します。
     _safeMint(msg.sender, newItemId);
 
     // tokenURIを更新します。
-    _setTokenURI(newItemId, finalTokenUri);
-
  	  // NFTがいつ誰に作成されたかを確認します。
 	  console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
@@ -133,5 +116,111 @@ event NewEpicNFTMinted(address sender, uint256 tokenId);
     _tokenIds.increment();
 
     emit NewEpicNFTMinted(msg.sender, newItemId);
+    return string(abi.encodePacked("data:application/json;base64,", json));
   }
+
+// function mintAndTransfer(
+//         string memory _credentialId,
+//         string memory _description,
+//         address[] memory _toAddresses,
+//         string[] memory _imageURIs,
+//         string[] memory _externalURIs
+//     ) public onlyOwner {
+//         uint256 requestNum = _toAddresses.length;
+//         require(requestNum > 0, "The _toAddresses is empty.");
+//         require(
+//             requestNum <= getMaxBatchSize(),
+//             "The length of _toAddresses must be less than or equal to _maxBatchSize."
+//         );
+//         require(
+//             requestNum == _imageURIs.length,
+//             "The length of _toAddresses and _imageURIs are NOT same."
+//         );
+//         require(
+//             requestNum == _externalURIs.length,
+//             "The length of _toAddresses and _externalURIs are NOT same."
+//         );
+//         for (uint i = 0; i < requestNum; i++) {
+//             address _to = _toAddresses[i];
+//             bool hasCredential = _credentialOwnerships[_credentialId][_to];
+//             require(
+//                 !hasCredential,
+//                 "One or more recipient has had already the same credential."
+//             );
+//             require(_to != owner(), "_toAddresses must NOT be included OWNER.");
+//         }
+
+//         // put the next token ID down in the variable before the bulk mint
+//         uint256 startTokenId = _nextTokenId();
+//         _safeMint(owner(), requestNum);
+
+//         // do bulk transfer to each specified address only for the minted tokens
+//         uint256 tokenId = startTokenId;
+//         for (uint i = 0; i < requestNum; i++) {
+//             // update the credential ID mapping
+//             _ownedCredential[tokenId] = _credentialId;
+//             // transfer to the specified address
+//             safeTransferFrom(owner(), _toAddresses[i], tokenId);
+//             // update the token URI
+//             _setTokenURI(
+//                 tokenId,
+//                 generateTokenURI(_description, _imageURIs[i], _externalURIs[i])
+//             );
+//             tokenId += 1;
+//         }
+//     }
+
+
+// =============================================================
+    //   The followings are copied from ERC721URIStorage.sol
+    // =============================================================
+
+    // Optional mapping for token URIs
+  // mapping(uint256 => string) private _tokenURIs;
+
+  // /**
+  //   * @dev See {IERC721Metadata-tokenURI}.
+  //   */
+  // function tokenURI(uint256 tokenId)
+  //     public
+  //     view
+  //     override
+  //     returns (string memory)
+  // {
+  //     require(_exists(tokenId));
+  //     string memory _tokenURI = _tokenURIs[tokenId];
+  //     string memory base = _baseURI();
+
+  //     // If there is no base URI, return the token URI.
+  //     if (bytes(base).length == 0) {
+  //         return _tokenURI;
+  //     }
+  //     // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+  //     if (bytes(_tokenURI).length > 0) {
+  //         return string(abi.encodePacked(base, _tokenURI));
+  //     }
+
+  //     return super.tokenURI(tokenId);
+  // }
+
+  // /**
+  //   * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+  //   *
+  //   * Requirements:
+  //   *
+  //   * - `tokenId` must exist.
+  //   */
+
+  // function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+  //       require(_exists(tokenId), "URI set of nonexistent token");
+  //       _tokenURIs[tokenId] = _tokenURI;
+  //   }
+  function transferOwnership(address newOwner) public override onlyOwner {
+        // Banned for transfering ownership to a user who has this token already,
+        // because to make sure that the status of credential ID mappings will not be complicated.
+        //require(balanceOf(newOwner) == 0, "newOwner's balance must be zero.");
+        super.transferOwnership(newOwner);
+    }
+
+  
 }
--- epic-nfts/scripts/deploy.js
+++ epic-nfts/scripts/deploy.js
@@ -2,7 +2,7 @@
 const main = async () => {
   // コントラクトがコンパイルします
   // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
-  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContractFactory = await hre.ethers.getContractFactory("mysbt");
   // Hardhat がローカルの Ethereum ネットワークを作成します。
   const nftContract = await nftContractFactory.deploy();
   // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
